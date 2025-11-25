0fc7eba0628828b658afcbd72ac0faafdf61d61f
yg-bit <jamesisaiah164@gmail.com>
2025-11-25 08:33:33 +0300

    added

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word)
+
+        for candidate in word_list:
+            cand_lower = candidate.lower()
+
+            # ignore identical word
+            if cand_lower == self.word:
+                continue
+
+            if sorted(cand_lower) == sorted_word:
+                matches.append(candidate)
+
+        return matches
